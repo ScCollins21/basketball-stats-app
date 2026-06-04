7744dc98b9fe22ab5f07bcf623a5bfd535fb2514
Spencer <spencer1.collins1@gmail.com>
2026-06-04 16:03:03 -0500

    Added player stat averages to playerstats controller

--- BasketballAPI/BasketballAPI/Controllers/PlayerStatsController.cs
+++ BasketballAPI/BasketballAPI/Controllers/PlayerStatsController.cs
@@ -1,6 +1,7 @@
 using BasketballAPI;
 using BasketballAPI.Database_Stuff;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BasketballAPI.Controllers
 {
@@ -12,16 +13,29 @@ namespace BasketballAPI.Controllers
 
     public PlayerStatsController(ApplicationDbContext context)
     {
-      _context = context;
+      this._context = context;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> PlayerStats(string playerId)
+    [HttpGet("{playerId}")]
+    public async Task<IActionResult> PlayerStatsCareerAvgs(int playerId)
     {
 
+      var averages = await _context.PlayerGameStats.Where(s => s.PersonId == playerId && s.GameType == "Regular Season").GroupBy(s => s.PersonId) // Group by player ID to calculate averages for the specified player
+        .Select(g => new
+        {
+          PlayerId = g.Key,
+          PointsPerGame = g.Average(s => s.Points),
+          AssistsPerGame = g.Average(s => s.Assists),
+          ReboundsPerGame = g.Average(s => s.ReboundsTotal),
+          StealsPerGame = g.Average(s => s.Steals),
+          BlocksPerGame = g.Average(s => s.Blocks)
+        }).FirstOrDefaultAsync();
 
+      if(averages == null) {
+        return NotFound("Player not found");
+      }
 
-      return Ok("Import complete");
+      return Ok(averages);
     }
   }
 }
